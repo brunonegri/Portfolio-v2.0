e2a3dddaeed2de988289ba82651f507be2dcd713
Bruno Negri <b.negri_@hotmail.com>
2022-08-11 05:00:57 -0300

    feat: add hamburguer menu

--- src/components/newHeader.jsx
+++ src/components/newHeader.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 import useScrollDirection from '../services/scrollhook'
 import GifBnWhite from '../data/images/GifBnWhite.gif'
 import BnWhite from '../data/images/BnWhite.png'
-import menuicon from '../data/StackIcons/menu_icon.png'
 
 function NewHeader () {
+	const [active, setActive] = useState(false)
+	const handleToggle = () => {
+		setActive(!active)
+	}
 	const scrollDirection = useScrollDirection()
 
 	return (
-		<div className={`w-full sm:sticky ${scrollDirection === 'down' ? '-top-24' : 'top-[30px]'}  flex flex-nowrap  justify-between sm:justify-center  drop-shadow-lg h-24 bg-[#464E59] z-10 transition-all duration-500`}>
+		<div className={`w-full sticky ${scrollDirection === 'down' ? '-top-24' : 'top-[30px]'}  flex flex-nowrap  justify-between sm:justify-center  drop-shadow-lg h-24 bg-[#464E59] z-10 transition-all duration-500`}>
 			<div className='w-[80%] lg:w-[65%] flex mx-auto justify-between items-center  h-full'>
 				<a href="#HOME">
-					<img className='h-10 sm:h-20'
+					<img className='h-16 sm:h-20'
 						onClick={() => { window.location.reload() }}
 						onMouseOver={e => (e.currentTarget.src = GifBnWhite)}
 						onMouseOut={e => (e.currentTarget.src = BnWhite)}
 						src={BnWhite} alt='Logo Bruno Negri' />
 				</a>
-				<button className='sm:hidden'>
-					<img src={menuicon} alt="menu" />
+				<button className={`sm:hidden ${active ? 'icon iconActive' : 'icon'}`} onClick={handleToggle}>
+					<div className='hamburguer hamburgerIcon'></div>
 				</button>
-				<nav className='hidden sm:flex lg:w-auto  text-white'>
+				<nav className= {`sm:flex lg:w-auto  backdrop-blur-md text-white ${active ? 'inset-y-0 right-[1px] top-[-30px] h-[4000px] w-56 bg-black/80 absolute h-full' : 'hidden'}`}>
 					<a href='/' onClick={ e=>{ 
 						const home = document.getElementById('HOME')
 						e.preventDefault()
 						home && home.scrollIntoView()
+						setActive(!active)
 						}}>
-						<p className='mx-2 text-xl hover:text-[#437c80]'>
+						<p className='ml-[30px] mt-[150px] sm:mx-2 sm:mt-0  text-xl hover:text-[#437c80]'>
 							<span className='mr-1 text-sm text-[#588e91]'>01.</span>Home
 						</p>
 					</a>
@@ -34,8 +38,9 @@ function NewHeader () {
 						const about = document.getElementById('ABOUT')
 						e.preventDefault()
 						about && about.scrollIntoView()
+						setActive(!active)
 						}}>
-						<p className='mx-2 text-xl hover:text-[#437c80]'>
+						<p className='ml-[30px] sm:mx-2 sm:mt-0 text-xl hover:text-[#437c80]'>
 							<span className='mr-1 text-sm text-[#588e91]'>02.</span>About
 						</p>
 					</a>
@@ -43,8 +48,9 @@ function NewHeader () {
 						const xp = document.getElementById('EXPERIENCES')
 						e.preventDefault()
 						xp && xp.scrollIntoView()
+						setActive(!active)
 						}}>
-						<p className='mx-2 text-xl hover:text-[#437c80]'>
+						<p className='ml-[30px] sm:mx-2 sm:mt-0 text-xl hover:text-[#437c80]'>
 							<span className='mr-1 text-sm text-[#588e91]'>03.</span>Experience
 						</p>
 					</a>
@@ -52,8 +58,9 @@ function NewHeader () {
 						const projects = document.getElementById('PROJECTS')
 						e.preventDefault()
 						projects && projects.scrollIntoView()
+						setActive(!active)
 						}}>
-						<p className='mx-2 text-xl hover:text-[#437c80]'>
+						<p className='ml-[30px] sm:mx-2 sm:mt-0 text-xl hover:text-[#437c80]'>
 							<span className='mr-1 text-sm text-[#588e91]'>04.</span>Projects
 						</p>
 					</a>
