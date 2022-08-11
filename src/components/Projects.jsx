b5ca81305ae9791ccdd747cba2d0bf49f266d2f4
Bruno Negri <b.negri_@hotmail.com>
2022-08-11 07:00:12 -0300

    fix: size img project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ function Projects () {
 	}
 
 	return (
-		<section className="sm:min-h-[850px] mb-20 mt-[90px] items-center">
+		<section className="sm:min-h-[850px] mb-20 sm:mb-0 mt-[90px] items-center">
 			<span className="anchorAbout" id='PROJECTS'></span>
 			<h1 className='text-4xl text-center mt-[190px] mb-5 sm:mb-9 text-[#4b8d91]'>Projects</h1>
 			<div className='flex items-center'>
@@ -26,7 +26,7 @@ function Projects () {
         i === current &&
 					<div key={i} className='flex flex-col sm:flex-row h-auto sm:h-[400px] w-full justify-around bg-[#313740] drop-shadow-xl items-center '>
 						<div className="flex sm:my-7 mt-10 sm:w-[45%] items-center justify-center ">
-							<img className='w-[80%]' src={e.thumb} alt="projectthumb" />
+							<img className='h-[200px] sm:h-[350px]' src={e.thumb} alt="projectthumb" />
 						</div>
 						<div className=' sm:w-[40%] text-3xl flex flex-col justify-center text-center mb-7'>
 							<p className='text-2xl sm:text-3xl text-center my-7 text-white'>{e.name}</p>
@@ -34,7 +34,7 @@ function Projects () {
 							<p className='mb-3 text-white text-xl sm:text-2xl'>{e.Stacks}</p>
 							<div className='mt-2 sm:mt-7'>
 								<a href={e.site} target='_blank' rel="noreferrer">
-									<FontAwesomeIcon href={e.site} target='_blank' className='h-10 sm:ml-5 text-white hover:text-[#437c80] transition-[0.5s]' icon={faLink}/>
+									<FontAwesomeIcon href={e.site} target='_blank' className='h-10 text-white hover:text-[#437c80] transition-[0.5s]' icon={faLink}/>
 								</a>
 								<a href={e.github} target='_blank' rel="noreferrer">
 									<FontAwesomeIcon href={e.github} target='_blank' className='h-10 ml-5 text-white hover:text-[#437c80] transition-[0.5s]' icon={faGithub}/>
